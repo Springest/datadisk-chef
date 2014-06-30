8a6ea5cde88fca9705f8a2b9fffb9ef66a308727
Wouter de Vos <wouter@springest.com>
2014-06-30 09:53:45 +0200

    Use storage provider in default cookbook as well.

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,30 +9,12 @@
 
 datadir = node['datadisk']['datapart']
 
-# target disk enumeration
-
 disks_with_fs = node[:filesystem].to_s.scan(/[sx]v?d[a-z]/)
 disks = node[:block_device].to_s.scan(/[sx]v?d[a-z]/)
 disk_without_fs = disks - disks_with_fs
-target_part = "/dev/" + disk_without_fs.join + "1"
 
-# TODO: handle >1 disk without FS
-# TODO: handle disk without partition
-
-execute "format target disk" do
-  command "mkfs -t ext3 /dev/#{disk_without_fs.join}1"
-  not_if { disk_without_fs.empty? || ::File.directory?("#{datadir}") }
-end
-
-#Create datadir
-
-execute "mkdir #{datadir}" do
-  not_if { ::File.directory?("#{datadir}") }
-end
-
-mount datadir do
-  device target_part
-  action [:mount, :enable]
+datadisk_storage datadir do
+  disk disk_without_fs.first
   fstype "ext3"
-  not_if { disk_without_fs.empty? }
+  action :create
 end
